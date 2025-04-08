69887aabd9f51a5729657b0ea5c23af09dc0292c
KsaVierCoder <ksavier06@interia.pl>
2025-04-08 18:48:53 +0200

    wip: data structure, commit get structures

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-
-users=[
-    {"name":"Beata","location":"Lublin","posts":500},
-    {"name":"Michał","location":"Krasnystaw","posts":420},
-    {"name":"Ksavier","location":"Grudziądz","posts":69},
-    {"name":"Damian","location":"Kraków","posts":2137},
+users = [
+    {"name": "Beata", "location": "Lublin", "posts": 500},
+    {"name": "Michał", "location": "Krasnystaw", "posts": 420},
+    {"name": "Ksavier", "location": "Grudziądz", "posts": 69},
+    {"name": "Damian", "location": "Kraków", "posts": 2137},
 
 ]
 
-for user in users:
-    print(f"Twój znajomy {user['name']}, z {user['location']} opublikował {user['posts']} postów")
+
+def get_user_info(users_data: list) -> None:
+    for user in users_data:
+        print(f"Twój znajomy {user['name']}, z {user['location']} opublikował {user['posts']} postów")
+
+
+get_user_info(users)
