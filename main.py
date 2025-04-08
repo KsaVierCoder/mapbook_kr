6a95b1a450afa7cfc05ee89d77943f8283790647
KsaVierCoder <ksavier06@interia.pl>
2025-04-08 19:01:01 +0200

    chore: mvt pattern

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-users = [
-    {"name": "Beata", "location": "Lublin", "posts": 500},
-    {"name": "Michał", "location": "Krasnystaw", "posts": 420},
-    {"name": "Ksavier", "location": "Grudziądz", "posts": 69},
-    {"name": "Damian", "location": "Kraków", "posts": 2137},
+from .utils.controller import get_user_info
+from .utils.model import users
 
-]
+def main():
+    get_user_info(users)
 
 
-def get_user_info(users_data: list) -> None:
-    for user in users_data:
-        print(f"Twój znajomy {user['name']}, z {user['location']} opublikował {user['posts']} postów")
 
-
-get_user_info(users)
+if __name__ == '__main__':
+    main()
