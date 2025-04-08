59a997938011006252f90715361ca95958eeb06d
KsaVierCoder <ksavier06@interia.pl>
2025-04-08 19:16:57 +0200

    wip: preparet menu

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from .utils.controller import get_user_info
-from .utils.model import users
+from ufo.controller import get_user_info
+from ufo.model import users
 
 def main():
-    get_user_info(users)
 
+
+    while True:
+        print("===============MENU================")
+        print('0 - zamknij aplikacje')
+        print('1 - wyświetl co u znajomych')
+        print('2 - dodaj nowego użytkowanika')
+        print('3 - usuń użytkownika')
+        print('4 - edytuj użytkownika')
+        print("===============MENU================")
+        choice = input('wybierz opcje menu')
+        if choice == '0':
+            break
+        if choice == '1':
+            get_user_info(users)
 
 
 if __name__ == '__main__':
